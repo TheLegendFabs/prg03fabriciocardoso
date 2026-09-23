18111d3801be19fcc4ce9caf8447dd399e588b9d
Fabricio <fabriciocardoso970@gmail.com>
2026-09-23 16:49:58 -0300

    feat: implementa relacionamentos, enum e testes da atividade 11

---
+++ prg03fabriciocardoso/src/main/java/br/com/ifba/usuario/entity/Perfil.java
@@ -0,0 +1,28 @@
+package br.com.ifba.usuario.entity;
+
+public class Perfil {
+
+    private String nome;
+    private String descricao;
+
+    public Perfil(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
